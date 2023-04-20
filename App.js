e562346b608d68e3858755e885364201e6718ea1
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-20 17:29:19 +0800

    feat: added authentication-provider

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
+import AuthenticationProvider from "./src/services/authentication-index";
 
 export default function App() {
   return (
-    <PaperProvider>
-      <StatusBar style="auto" />
-    </PaperProvider>
+    <AuthenticationProvider>
+      <PaperProvider>
+        <StatusBar style="auto" />
+      </PaperProvider>
+    </AuthenticationProvider>
   );
 }
