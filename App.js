5af4a08fdbc6b6655af3f597aa6380524de9bb9e
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-21 22:33:15 +0800

    feat: added NavigationIndex

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 import AuthenticationProvider from "./src/services/authentication-index";
+import NavigationIndex from "./src/infrastructure/navigation/navigation-index";
 
 export default function App() {
   return (
     <AuthenticationProvider>
       <PaperProvider>
+        <NavigationIndex/>
         <StatusBar style="auto" />
       </PaperProvider>
     </AuthenticationProvider>
