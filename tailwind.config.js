2fa5905e43aa9c3febec571d5d6abb887abdfa42
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-21 23:17:42 +0800

    chore: extended colors using the custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("./src/infrastructure/themes/colors.js");
 module.exports = {
   content: ["./App.js", "./src/components/*.jsx", "./src/features/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   plugins: [],
 };
