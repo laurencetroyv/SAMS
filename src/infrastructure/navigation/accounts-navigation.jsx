98aeae6f8b893d4452de26ae5ae643200d3643ec
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-22 00:08:55 +0800

    feat: added sign-in-screen and hide header

--- src/infrastructure/navigation/accounts-navigation.jsx
+++ src/infrastructure/navigation/accounts-navigation.jsx
@@ -1,11 +1,18 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import React from 'react'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import SignIn from "../../features/accounts/sign-in-screen";
+
+const SignInScreen = () => <SignIn />;
 
 export default function AccountsNavigation() {
-  const Stack = createNativeStackNavigator()
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name='null' component={null} />
-        </Stack.Navigator>
-  )
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Sign-in" component={SignInScreen} />
+    </Stack.Navigator>
+  );
 }
