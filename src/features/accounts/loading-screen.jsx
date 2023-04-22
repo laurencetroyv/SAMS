0fb0e1eff6611d6a72d105fa7b349cff06c1cec4
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-22 08:55:51 +0800

    feat: remove container

--- src/features/accounts/loading-screen.jsx
+++ src/features/accounts/loading-screen.jsx
@@ -4,15 +4,13 @@ import { View } from "react-native";
 import Lottie from "lottie-react-native";
 export default function Loading() {
   return (
-    <Container>
-      <View className="flex-1 items-center justify-center">
-        <Lottie
-          className="w-full"
-          source={require("../../assets/lottie-loading.json")}
-          autoPlay
-          loop
-        />
-      </View>
-    </Container>
+    <View className="flex-1 items-center justify-center">
+      <Lottie
+        className="w-full"
+        source={require("../../assets/lottie-loading.json")}
+        autoPlay
+        loop
+      />
+    </View>
   );
 }
