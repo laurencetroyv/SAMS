f33172fd99e871427d0e3e22494d28e375941d35
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-22 08:55:31 +0800

    feat: remove id-number input, added email-input & loading animation

--- src/features/accounts/sign-in-screen.jsx
+++ src/features/accounts/sign-in-screen.jsx
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
-import { Divider, Text, TextInput } from "react-native-paper";
+import React, { useContext, useState } from "react";
+import { Divider, Modal, Portal, Text, TextInput } from "react-native-paper";
 import { TouchableOpacity, View } from "react-native";
 import { SignInIllustration } from "../../assets/assets-index";
 import {
   Container,
-  StyledTextInput as IdNumberInput,
+  StyledTextInput as EmailInput,
   StyledTextInput as PasswordInput,
   StyledButton,
 } from "../../components/components-index";
 import { useNavigation } from "@react-navigation/native";
+import { AuthenticationContext } from "../../services/authentication-index";
+import Loading from "./loading-screen";
 
 export default function SignIn() {
   const navigation = useNavigation();
+  const { loginRequest, loading } = useContext(AuthenticationContext);
 
-  const [IdNumber, setIdNumber] = useState("");
-  const [IdNumberError, setIdNumberError] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
 
   const [password, setPassword] = useState("");
   const [passwordError, setPasswordError] = useState(false);
@@ -22,73 +25,90 @@ export default function SignIn() {
 
   return (
     <Container>
-      <View>
-        <SignInIllustration />
-      </View>
+      {loading ? (
+        <Loading />
+      ) : (
+        <View>
+          <View>
+            <SignInIllustration />
+          </View>
 
-      <Divider className="my-8" />
+          <Divider className="my-8" />
 
-      <View className="mb-4">
-        <Text className="text-primary text-3xl font-bold">Hello</Text>
-        <Text className="font-bold text-3xl">Login now.</Text>
-        <Text className="w-3/4">
-          Welcome back. Please fill in the form to sign in and continue
-        </Text>
-      </View>
+          <View className="mb-4">
+            <Text className="text-primary text-3xl font-bold">Hello</Text>
+            <Text className="font-bold text-3xl">Login now.</Text>
+            <Text className="w-3/4">
+              Welcome back. Please fill in the form to sign in and continue
+            </Text>
+          </View>
 
-      <IdNumberInput
-        label={"ID Number"}
-        autoComplete="email"
-        inputMode="email"
-        keyboardType="email-address"
-        onChangeText={setIdNumber}
-        onEndEditing={() =>
-          IdNumber.length !== 9
-            ? setIdNumberError(true)
-            : setIdNumberError(false)
-        }
-        placeholder="2020-0001"
-        textContentType={"emailAddress"}
-        right={<TextInput.Icon icon={"email"} forceTextInputFocus={false} />}
-        showHelper={IdNumberError}
-        helperMessage={"Invalid ID Number"}
-        maxLength={9}
-      />
-      <PasswordInput
-        label={"Password"}
-        autoComplete={"password"}
-        autoCapitalize={"none"}
-        inputMode={"text"}
-        onChangeText={setPassword}
-        placeholder={"P@ssw0rd"}
-        secureTextEntry={passwordSecure}
-        textContentType={"password"}
-        right={
-          <TextInput.Icon
-            icon={passwordSecure ? "eye" : "eye-outline"}
-            onPress={() => setPasswordSecure(!passwordSecure)}
-            forceTextInputFocus={false}
+          <EmailInput
+            label={"Email Address"}
+            autoComplete="email"
+            inputMode="email"
+            keyboardType="email-address"
+            onChangeText={setEmail}
+            onEndEditing={() =>
+              !email.includes("@g.msuiit.edu.ph")
+                ? setEmailError(true)
+                : setEmailError(false)
+            }
+            placeholder="@g.msuiit.edu.ph"
+            textContentType={"emailAddress"}
+            right={
+              <TextInput.Icon icon={"email"} forceTextInputFocus={false} />
+            }
+            error={emailError}
+            errorMessage={"Invalid email address"}
+            maxLength={64}
           />
-        }
-        error={passwordError}
-        errorMessage={"Invalid password"}
-        passwordRules="minlength: 8; required: lower; required: upper; required: digit; required: [-];"
-      />
 
-      <StyledButton title={"Sign In"} />
+          <PasswordInput
+            label={"Password"}
+            autoComplete={"password"}
+            autoCapitalize={"none"}
+            inputMode={"text"}
+            onChangeText={setPassword}
+            placeholder={"P@ssw0rd"}
+            secureTextEntry={passwordSecure}
+            textContentType={"password"}
+            right={
+              <TextInput.Icon
+                icon={passwordSecure ? "eye" : "eye-outline"}
+                onPress={() => setPasswordSecure(!passwordSecure)}
+                forceTextInputFocus={false}
+              />
+            }
+            error={passwordError}
+            errorMessage={"Invalid password"}
+            passwordRules="minlength: 8; required: lower; required: upper; required: digit; required: [-];"
+          />
 
-      <View className="my-4 space-y-4">
-        <TouchableOpacity onPress={() => navigation.navigate("Sign-up")}>
-          <Text className="text-center">
-            Don't have an account?{" "}
-            <Text className="font-bold">Register Here</Text>
-          </Text>
-        </TouchableOpacity>
+          <View>
+            <TouchableOpacity
+              onPress={() => {
+                loginRequest(email, password);
+              }}
+            >
+              <StyledButton title={"Sign In"} />
+            </TouchableOpacity>
+          </View>
 
-        <Text className="text-center">
-          Forgot Password? <Text className="font-bold">Register Here</Text>
-        </Text>
-      </View>
+          <View className="my-4 space-y-4">
+            <TouchableOpacity onPress={() => navigation.navigate("Sign-up")}>
+              <Text className="text-center">
+                Don't have an account?{" "}
+                <Text className="font-bold">Register Here</Text>
+              </Text>
+            </TouchableOpacity>
+
+            <Text className="text-center">
+              Forgot Password? <Text className="font-bold">Register Here</Text>
+            </Text>
+          </View>
+        </View>
+      )}
     </Container>
   );
 }
