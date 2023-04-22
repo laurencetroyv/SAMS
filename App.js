2e3838d09832ff251edd87d6b087c9c82d4e6eb4
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-22 23:55:49 +0800

    feat: removed student-provider

--- App.js
+++ App.js
@@ -3,17 +3,14 @@ import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 import AuthenticationProvider from "./src/services/authentication-index";
 import NavigationIndex from "./src/infrastructure/navigation/navigation-index";
-import StudentProvider from "./src/services/student-index";
 
 export default function App() {
   return (
-    <StudentProvider>
-      <AuthenticationProvider>
-        <PaperProvider>
-          <NavigationIndex />
-          <StatusBar style="auto" />
-        </PaperProvider>
-      </AuthenticationProvider>
-    </StudentProvider>
+    <AuthenticationProvider>
+      <PaperProvider>
+        <NavigationIndex />
+        <StatusBar style="auto" />
+      </PaperProvider>
+    </AuthenticationProvider>
   );
 }
