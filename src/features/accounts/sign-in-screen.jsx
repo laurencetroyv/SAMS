226bbbaf8c62ac3199acebe234c41d544854f2c7
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-22 09:30:08 +0800

    feat: revert back to lottie loading animation

--- src/features/accounts/sign-in-screen.jsx
+++ src/features/accounts/sign-in-screen.jsx
@@ -15,6 +15,7 @@ import {
 import { useNavigation } from "@react-navigation/native";
 import { AuthenticationContext } from "../../services/authentication-index";
 import Lottie from "lottie-react-native";
+import Loading from "./loading-screen";
 
 export default function SignIn() {
   const navigation = useNavigation();
@@ -35,89 +36,89 @@ export default function SignIn() {
 
   return (
     <Container>
-      <View>
+      {loading ? (
+        <Loading />
+      ) : (
         <View>
-          <Lottie
-            className="w-full"
-            source={require("../../assets/lottie-chill-sign-in.json")}
-            autoPlay
-            loop
-          />
-        </View>
-
-        <Divider className="my-8" />
-
-        <View className="mb-4">
-          <Text className="text-primary text-3xl font-bold">Hello</Text>
-          <Text className="font-bold text-3xl">Login now.</Text>
-          <Text className="w-3/4">
-            Welcome back. Please fill in the form to sign in and continue
-          </Text>
-        </View>
-
-        <EmailInput
-          label={"Email Address"}
-          autoComplete="email"
-          inputMode="email"
-          keyboardType="email-address"
-          onChangeText={setEmail}
-          onEndEditing={() =>
-            !email.includes("@g.msuiit.edu.ph")
-              ? setEmailError(true)
-              : setEmailError(false)
-          }
-          placeholder="@g.msuiit.edu.ph"
-          textContentType={"emailAddress"}
-          right={<TextInput.Icon icon={"email"} forceTextInputFocus={false} />}
-          error={emailError}
-          errorMessage={"Invalid email address"}
-          maxLength={64}
-        />
-
-        <PasswordInput
-          label={"Password"}
-          autoComplete={"password"}
-          autoCapitalize={"none"}
-          inputMode={"text"}
-          onChangeText={setPassword}
-          placeholder={"P@ssw0rd"}
-          secureTextEntry={passwordSecure}
-          textContentType={"password"}
-          right={
-            <TextInput.Icon
-              icon={passwordSecure ? "eye" : "eye-outline"}
-              onPress={() => setPasswordSecure(!passwordSecure)}
-              forceTextInputFocus={false}
+          <View>
+            <Lottie
+              className="w-full"
+              source={require("../../assets/lottie-chill-sign-in.json")}
+              autoPlay
+              loop
             />
-          }
-          error={passwordError}
-          errorMessage={"Invalid password"}
-          passwordRules="minlength: 8; required: lower; required: upper; required: digit; required: [-];"
-        />
+          </View>
 
-        <View>
-          {loading ? (
-            <ActivityIndicator />
-          ) : (
-            <TouchableOpacity onPress={() => signIn()}>
-              <StyledButton title={"Sign In"} />
-            </TouchableOpacity>
-          )}
-        </View>
+          <Divider className="my-8" />
 
-        <View className="my-4 space-y-4">
-          <TouchableOpacity onPress={() => navigation.navigate("Sign-up")}>
-            <Text className="text-center">
-              Don't have an account?{" "}
-              <Text className="font-bold">Register Here</Text>
+          <View className="mb-4">
+            <Text className="text-primary text-3xl font-bold">Hello</Text>
+            <Text className="font-bold text-3xl">Login now.</Text>
+            <Text className="w-3/4">
+              Welcome back. Please fill in the form to sign in and continue
             </Text>
+          </View>
+
+          <EmailInput
+            label={"Email Address"}
+            autoComplete="email"
+            inputMode="email"
+            keyboardType="email-address"
+            onChangeText={setEmail}
+            onEndEditing={() =>
+              !email.includes("@g.msuiit.edu.ph")
+                ? setEmailError(true)
+                : setEmailError(false)
+            }
+            placeholder="@g.msuiit.edu.ph"
+            textContentType={"emailAddress"}
+            right={
+              <TextInput.Icon icon={"email"} forceTextInputFocus={false} />
+            }
+            error={emailError}
+            errorMessage={"Invalid email address"}
+            maxLength={64}
+          />
+
+          <PasswordInput
+            label={"Password"}
+            autoComplete={"password"}
+            autoCapitalize={"none"}
+            inputMode={"text"}
+            onChangeText={setPassword}
+            placeholder={"P@ssw0rd"}
+            secureTextEntry={passwordSecure}
+            textContentType={"password"}
+            right={
+              <TextInput.Icon
+                icon={passwordSecure ? "eye" : "eye-outline"}
+                onPress={() => setPasswordSecure(!passwordSecure)}
+                forceTextInputFocus={false}
+              />
+            }
+            error={passwordError}
+            errorMessage={"Invalid password"}
+            passwordRules="minlength: 8; required: lower; required: upper; required: digit; required: [-];"
+          />
+
+          <TouchableOpacity onPress={() => signIn()}>
+            <StyledButton title={"Sign In"} />
           </TouchableOpacity>
 
-          <Text className="text-center">
-            Forgot Password? <Text className="font-bold">Register Here</Text>
-          </Text>
+          <View className="my-4 space-y-4">
+            <TouchableOpacity onPress={() => navigation.navigate("Sign-up")}>
+              <Text className="text-center">
+                Don't have an account?{" "}
+                <Text className="font-bold">Register Here</Text>
+              </Text>
+            </TouchableOpacity>
+
+            <Text className="text-center">
+              Forgot Password? <Text className="font-bold">Register Here</Text>
+            </Text>
+          </View>
         </View>
-      </View>
+      )}
     </Container>
   );
 }
--- src/features/accounts/sign-up-screen.jsx
+++ src/features/accounts/sign-up-screen.jsx
@@ -17,6 +17,7 @@ import {
 import { useNavigation } from "@react-navigation/native";
 import { AuthenticationContext } from "../../services/authentication-index";
 import Lottie from "lottie-react-native";
+import Loading from "./loading-screen";
 
 export default function SignUp() {
   const navigation = useNavigation();
@@ -42,119 +43,123 @@ export default function SignUp() {
 
   return (
     <Container>
-      <View>
-        <View className="items-center">
-          <Lottie
-            className="w-full"
-            source={require("../../assets/lottie-chill.json")}
-            autoPlay
-            loop
-          />
-        </View>
-
-        <Divider className="my-8" />
-
-        <View className="mb-4">
-          <Text className="text-primary text-3xl font-bold">Hello</Text>
-          <Text className="font-bold text-3xl">Create account</Text>
-          <Text className="font-bold text-3xl">now.</Text>
-          <Text className="w-3/4">
-            Please fill in the form to create a new account and continue
-          </Text>
-        </View>
-
-        <IdNumberInput
-          label={"ID Number"}
-          autoComplete="email"
-          inputMode="email"
-          keyboardType="email-address"
-          onChangeText={setIdNumber}
-          onEndEditing={() =>
-            IdNumber.length !== 9
-              ? setIdNumberError(true)
-              : setIdNumberError(false)
-          }
-          placeholder="2020-0001"
-          textContentType={"emailAddress"}
-          right={<TextInput.Icon icon={"email"} forceTextInputFocus={false} />}
-          showHelper={IdNumberError}
-          helperMessage={"Invalid ID Number"}
-          maxLength={9}
-        />
-
-        <EmailInput
-          label={"Email Address"}
-          autoComplete="email"
-          inputMode="email"
-          keyboardType="email-address"
-          onChangeText={setEmail}
-          onEndEditing={() =>
-            !email.includes("@g.msuiit.edu.ph")
-              ? setEmailError(true)
-              : setEmailError(false)
-          }
-          placeholder="@g.msuiit.edu.ph"
-          textContentType={"emailAddress"}
-          right={<TextInput.Icon icon={"email"} forceTextInputFocus={false} />}
-          error={emailError}
-          errorMessage={"Invalid email address"}
-          maxLength={64}
-        />
-
-        <PasswordInput
-          label={"Password"}
-          autoComplete={"password"}
-          autoCapitalize={"none"}
-          inputMode={"text"}
-          onChangeText={setPassword}
-          placeholder={"P@ssw0rd"}
-          secureTextEntry={passwordSecure}
-          textContentType={"password"}
-          right={
-            <TextInput.Icon
-              icon={passwordSecure ? "eye" : "eye-outline"}
-              onPress={() => setPasswordSecure(!passwordSecure)}
-              forceTextInputFocus={false}
+      {loading ? (
+        <Loading />
+      ) : (
+        <View>
+          <View className="items-center">
+            <Lottie
+              className="w-full"
+              source={require("../../assets/lottie-chill.json")}
+              autoPlay
+              loop
             />
-          }
-          error={passwordError}
-          errorMessage={"Invalid password"}
-          passwordRules="minlength: 8; required: lower; required: upper; required: digit; required: [-];"
-        />
+          </View>
 
-        <View className="flex-row items-center space-x-4 w-3/4 mt-2">
-          <Checkbox
-            status={checked ? "checked" : "unchecked"}
-            onPress={() => setChecked(!checked)}
+          <Divider className="my-8" />
+
+          <View className="mb-4">
+            <Text className="text-primary text-3xl font-bold">Hello</Text>
+            <Text className="font-bold text-3xl">Create account</Text>
+            <Text className="font-bold text-3xl">now.</Text>
+            <Text className="w-3/4">
+              Please fill in the form to create a new account and continue
+            </Text>
+          </View>
+
+          <IdNumberInput
+            label={"ID Number"}
+            autoComplete="email"
+            inputMode="email"
+            keyboardType="email-address"
+            onChangeText={setIdNumber}
+            onEndEditing={() =>
+              IdNumber.length !== 9
+                ? setIdNumberError(true)
+                : setIdNumberError(false)
+            }
+            placeholder="2020-0001"
+            textContentType={"emailAddress"}
+            right={
+              <TextInput.Icon icon={"email"} forceTextInputFocus={false} />
+            }
+            showHelper={IdNumberError}
+            helperMessage={"Invalid ID Number"}
+            maxLength={9}
           />
-          <TouchableOpacity>
-            <Text>
-              I agree to the{" "}
-              <Text className="text-primary">terms & conditions</Text> and{" "}
-              <Text className="text-primary">privacy policy</Text>
-            </Text>
-          </TouchableOpacity>
-        </View>
 
-        <View className="my-4 space-y-4">
-          <TouchableOpacity onPress={() => register()}>
-            <Text className="text-center">
-              Already have an account?{" "}
-              <Text className="font-bold text-primary">Click Here</Text>
-            </Text>
-          </TouchableOpacity>
+          <EmailInput
+            label={"Email Address"}
+            autoComplete="email"
+            inputMode="email"
+            keyboardType="email-address"
+            onChangeText={setEmail}
+            onEndEditing={() =>
+              !email.includes("@g.msuiit.edu.ph")
+                ? setEmailError(true)
+                : setEmailError(false)
+            }
+            placeholder="@g.msuiit.edu.ph"
+            textContentType={"emailAddress"}
+            right={
+              <TextInput.Icon icon={"email"} forceTextInputFocus={false} />
+            }
+            error={emailError}
+            errorMessage={"Invalid email address"}
+            maxLength={64}
+          />
 
-          {loading ? (
-            <ActivityIndicator />
-          ) : (
+          <PasswordInput
+            label={"Password"}
+            autoComplete={"password"}
+            autoCapitalize={"none"}
+            inputMode={"text"}
+            onChangeText={setPassword}
+            placeholder={"P@ssw0rd"}
+            secureTextEntry={passwordSecure}
+            textContentType={"password"}
+            right={
+              <TextInput.Icon
+                icon={passwordSecure ? "eye" : "eye-outline"}
+                onPress={() => setPasswordSecure(!passwordSecure)}
+                forceTextInputFocus={false}
+              />
+            }
+            error={passwordError}
+            errorMessage={"Invalid password"}
+            passwordRules="minlength: 8; required: lower; required: upper; required: digit; required: [-];"
+          />
+
+          <View className="flex-row items-center space-x-4 w-3/4 mt-2">
+            <Checkbox
+              status={checked ? "checked" : "unchecked"}
+              onPress={() => setChecked(!checked)}
+            />
+            <TouchableOpacity>
+              <Text>
+                I agree to the{" "}
+                <Text className="text-primary">terms & conditions</Text> and{" "}
+                <Text className="text-primary">privacy policy</Text>
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="my-4 space-y-4">
+            <TouchableOpacity onPress={() => register()}>
+              <Text className="text-center">
+                Already have an account?{" "}
+                <Text className="font-bold text-primary">Click Here</Text>
+              </Text>
+            </TouchableOpacity>
+
             <TouchableOpacity
               onPress={() => registerRequest(IdNumber, email, password)}
             >
               <StyledButton title={"Sign Up"} />
             </TouchableOpacity>
-          )}
+          </View>
         </View>
-      </View>
+      )}
     </Container>
   );
 }
