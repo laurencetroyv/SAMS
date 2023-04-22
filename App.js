8eb6baf37e4f1e5495ed3da10da221994aed082f
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-22 09:24:28 +0800

    feat: added student provider

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 import AuthenticationProvider from "./src/services/authentication-index";
 import NavigationIndex from "./src/infrastructure/navigation/navigation-index";
+import StudentProvider from "./src/services/student-index";
 
 export default function App() {
   return (
-    <AuthenticationProvider>
-      <PaperProvider>
-        <NavigationIndex/>
-        <StatusBar style="auto" />
-      </PaperProvider>
-    </AuthenticationProvider>
+    <StudentProvider>
+      <AuthenticationProvider>
+        <PaperProvider>
+          <NavigationIndex />
+          <StatusBar style="auto" />
+        </PaperProvider>
+      </AuthenticationProvider>
+    </StudentProvider>
   );
 }
