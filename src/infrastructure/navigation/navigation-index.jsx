e25dfa1e944ae2f1f4bc379beddde4f7ef98d085
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-22 23:59:33 +0800

    feat: student-provider now wrap the main navigation-index

--- src/infrastructure/navigation/navigation-index.jsx
+++ src/infrastructure/navigation/navigation-index.jsx
@@ -4,15 +4,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import AccountsNavigation from "./accounts-navigation";
 import AppNavigation from "./app-navigation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import StudentProvider from "../../services/student-index";
 
 export default function NavigationIndex() {
   const { isAuthenticated } = useContext(AuthenticationContext);
 
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        {isAuthenticated ? <AppNavigation /> : <AccountsNavigation />}
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <StudentProvider>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          {isAuthenticated ? <AppNavigation /> : <AccountsNavigation />}
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </StudentProvider>
   );
 }
